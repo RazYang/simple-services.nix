d06a4b997ad7d18e906c721cfe0958c470eefe5a
RazYang <xzzorz@gamil.com>
2025-12-25 15:25:29 +0800

    展示infuse的不同两种写法

--- packages/redis/package.nix
+++ packages/redis/package.nix
@@ -1,18 +1,19 @@
 # 这是一个拥有独立作用域的，使用callPackage设计模式的软件包定义函数，与nixpkgs中软件包的定义方式基本相同：
 # https://github.com/NixOS/nixpkgs/blob/nixos-25.11/pkgs/by-name/he/hello/package.nix
+#
+# infuse: 来自 overlays/default.nix，通过 overlay 机制被添加到 pkgs 中
+# pkgs: pkgs本体，因为这个包也叫redis，使用pkgs.redis避免循环引用
 { infuse, pkgs, ... }:
-#   - infuse: 来自 overlays/default.nix，通过 overlay 机制被添加到 pkgs 中
-#   - pkgs: pkgs本体，因为这个包也叫redis，使用pkgs.redis避免循环引用
 infuse pkgs.redis {
   # 输入参数覆盖：效果等同于override
   __input = {
-    withSystemd = _: false;
-    tlsSupport = _: false;
-    useSystemJemalloc = _: false;
+    tlsSupport.__assign = false;
+    withSystemd.__assign = false;
+    useSystemJemalloc = _: false; # 等价于useSystemJemalloc.__assign = false;
   };
   # 属性覆盖：效果等同于overrideAttrs
   __output = {
-    doCheck = _: false;
+    doCheck.__assign = false;
     makeFlags.__append = [ "MALLOC=libc" ];
   };
 }
